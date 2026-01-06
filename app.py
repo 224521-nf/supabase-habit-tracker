db941a36363f645df2b01bd804303de93845f216
fumiya nagumo <kcit.fumiya.nagumo@gmail.com>
2026-01-06 12:03:17 +0900

    update app

--- app.py
+++ app.py
@@ -479,7 +479,7 @@ def render_challenge(user_id):
             # リセット処理
             tracker.reset_logs(user_id)
 
-            st.markdown("### 習慣の連続日数が２日間連続で更新されなかったため、日数を０に初期化しました。")   
+            st.markdown("### 習慣の連続日数が２日間更新されなかったため、日数を０に初期化しました。")   
             st.write("")
             st.markdown("<p color: #999;'>💡 習慣を続けるコツ: ハードルを下げて、毎日続けやすい内容にしましょう</p>", unsafe_allow_html=True)
             st.write("")
