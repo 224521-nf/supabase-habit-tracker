bb2cd39ef288237e08c2c7262d4b719f61c2d792
fumiya nagumo <kcit.fumiya.nagumo@gmail.com>
2026-01-08 15:42:41 +0900

    update app

--- app.py
+++ app.py
@@ -456,7 +456,7 @@ def render_challenge(user_id):
             )
            
         st.write("")
-        st.markdown("<p style='color: #999; font-weight: bold; text-align: center; font-size: 22px;'>💡 ハードルを下げて、毎日続けやすい内容にしましょう</p>", unsafe_allow_html=True)
+        st.markdown("<p style='color: #999; font-weight: bold; text-align: center; font-size: 18px;'>💡 ハードルを下げて、毎日続けやすい内容にしましょう</p>", unsafe_allow_html=True)
         st.write("")
         st.write("")
         
