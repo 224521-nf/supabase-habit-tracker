0934275591de2cdf6b01e3a1e9b6efc3d26cacbf
fumiya nagumo <kcit.fumiya.nagumo@gmail.com>
2026-01-08 14:59:05 +0900

    update app

--- app.py
+++ app.py
@@ -443,12 +443,12 @@ def render_challenge(user_id):
     if should_show_reset:
         st.markdown(f"<h1 style='text-align: center;'>🎯 {habit['name']}</h1>", unsafe_allow_html=True)
         st.markdown(f"<h3 style = 'text-align : center;'> 目標時間: {habit["target_time"]}</h3>",unsafe_allow_html=True)
+        st.markdown(" ")
         
         st.markdown(
     """
-    <p style="text-align: center; font-weight: bold; font-size: 35px;">
-        ⚠️ 習慣の連続日数が2日間更新されなかったため、<br>
-        日数が0に初期化されました。
+    <p style="text-align: center; font-weight: bold; font-size: 33px;">
+        習慣の連続日数が2日間更新されなかったため、日数が0に初期化されました。
     </p>
     """,
     unsafe_allow_html=True
