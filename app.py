bf072ca921ed21c872965221bfc607874f71c6b2
fumiya nagumo <kcit.fumiya.nagumo@gmail.com>
2026-01-09 10:04:06 +0900

    update app

--- app.py
+++ app.py
@@ -230,6 +230,7 @@ def render_progress_chart(logs, max_days=30):
         return
     
     df = pd.DataFrame(logs)
+    st.write(df[["log_date", "completion_hour"]].head())
     df["log_date"] = pd.to_datetime(df["log_date"])
     df = df.sort_values(by="log_date").tail(max_days)
     
