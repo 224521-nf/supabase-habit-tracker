8d07037e52be38d2ef59aa2f1d63ccbecb37f853
fumiya nagumo <kcit.fumiya.nagumo@gmail.com>
2026-01-13 14:02:39 +0900

    update app

--- app.py
+++ app.py
@@ -229,7 +229,10 @@ def render_progress_chart(logs, max_days=30):
         return
     
     df = pd.DataFrame(logs)
-    st.write(df[["log_date", "completion_hour"]])
+    
+    with st.expander("1クリックごとの達成時間", expanded=False):
+        st.write(df[["log_date", "completion_hour"]])
+        
     df["log_date"] = pd.to_datetime(df["log_date"])
     df = df.sort_values(by="log_date").tail(max_days)
     
